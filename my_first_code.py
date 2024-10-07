fe9583f3447afe5dc1449d58f90990317a07b8b3
ThatsKempo <joedavidkempson@gmail.com>
2024-10-07 09:17:30 +0100

    Hello World

---
+++ my_first_code.py
@@ -0,0 +1,32 @@
+print("Okay this... is epic.")
+print("Wow this", "is insane!", 2024)
+print("Hello", end=" epic ")
+print("World")
+print("Foot" + "ball")
+
+name = "Larry"
+age = 23
+trait = "funny"
+print(f"{name} is {age} years old \n{name} is {trait}")
+
+print(5022-901)
+print(5*10)
+print(604/4)
+print(3%2) # MOD % - Returns the remainder of division
+print(10%4)
+print(9//3) # DIV //  - Returns the the Interger before the decimal point
+print(8//3)
+print(5**2) # Exponent
+print(4**3)
+
+'''
+a = int(input("Enter num 1"))
+b = int(input("Enter num 2"))
+result = a + b
+print(result) # f2 to change every varible name
+'''
+
+a = (6*4)
+b = (5**2)
+c = (4%3)
+print(f"{a} + {b} - {c} =",a + b - c)
